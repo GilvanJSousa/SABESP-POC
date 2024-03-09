2bed81bc962563f93f7b842596818b927bc6eff0
GilvanS <gillvanjs@gmail.com>
2024-03-09 20:43:43 -0300

    teste

---
+++ cypress.config.js
@@ -0,0 +1,16 @@
+const cucumber = require('cypress-cucumber-preprocessor').default
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    defaultCommandTimeout: 4000,
+    pageLoadTimeout: 30000,
+    video: true,
+    viewportWidth: 1280,
+    viewportHeight: 720,
+    setupNodeEvents(on, config) {
+      on('file:preprocessor', cucumber())
+     }, 
+     specPattern: "cypress/e2e/step_definitions/*.feature"
+  },
+});
